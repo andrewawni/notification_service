6d09791fe2db57f96a45f7f4571559d1261c99c1
andrewawni <andrewawnii@gmail.com>
2021-03-21 23:39:52 +0200

    [feat]: implement endpoint handlers in api service

--- api/app.go
+++ api/app.go
@@ -4,17 +4,28 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/andrewawni/notification_service/common/messaging"
+	"github.com/andrewawni/notification_service/common/models"
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 )
 
 type App struct {
-	router *mux.Router
+	router          *mux.Router
+	messagingClient messaging.IMessagingClient
 }
+
+const singleNotificationQueueName = "notification_service:assemble_single_notifications_jobs"
+const groupNotificationQueueName = "notification_service:assemble_group_notifications_jobs"
 
 func (app *App) Init() {
 	app.router = mux.NewRouter()
 	app.drawRoutes()
+	app.messagingClient = &messaging.MessagingClient{}
+	app.messagingClient.ConnectToBroker(os.Getenv("RABBITMQ_URL"))
+	log.Printf("connected to rabbitmq")
 }
 
 func (app *App) Run(address string) {
@@ -33,8 +44,39 @@ func respondWithError(w http.ResponseWriter, code int, message string) {
 }
 
 func (app *App) drawRoutes() {
-	app.router.HandleFunc("/sendNotification", app.sendNotificationHandler).Methods("POST")
+	app.router.HandleFunc("/", app.index).Methods("POST")
+	app.router.HandleFunc("/sendSingleNotification", app.sendSingleNotificationHandler).Methods("POST")
+	app.router.HandleFunc("/sendGroupNotification", app.sendGroupNotificationHandler).Methods("POST")
 }
 
-func (app *App) sendNotificationHandler(w http.ResponseWriter, r *http.Request) {
+func (app *App) index(w http.ResponseWriter, r *http.Request) {
+	log.Printf("I'm working")
+}
+
+func (app *App) sendSingleNotificationHandler(w http.ResponseWriter, r *http.Request) {
+	id := uuid.New().String()
+	body := models.SingleNotification{}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid payload")
+		return
+	}
+	body.ID = id
+	payload, _ := json.Marshal(&body)
+	app.messagingClient.PublishOnQueue([]byte(payload), singleNotificationQueueName)
+	respondWithJSON(w, http.StatusAccepted, body)
+}
+
+func (app *App) sendGroupNotificationHandler(w http.ResponseWriter, r *http.Request) {
+	id := uuid.New().String()
+	body := models.GroupNotification{}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid payload")
+		return
+	}
+	body.ID = id
+	payload, _ := json.Marshal(&body)
+	app.messagingClient.PublishOnQueue([]byte(payload), groupNotificationQueueName)
+	respondWithJSON(w, http.StatusAccepted, body)
 }
--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,8 @@ module github.com/andrewawni/notification_service/api
 go 1.15
 
 require (
-	github.com/andrewawni/notification_service v0.0.0-20210319223304-4563c62c1730
+	github.com/andrewawni/notification_service/common v0.0.0-20210321191836-92b85f1565b2
+	github.com/google/uuid v1.2.0
 	github.com/gorilla/mux v1.8.0
+	google.golang.org/genproto v0.0.0-20210319143718-93e7006c17a6
 )
